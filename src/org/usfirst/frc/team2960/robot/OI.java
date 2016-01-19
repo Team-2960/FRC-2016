6b127241915616ed589eb652bb094bf4daa23887
Eric <EKS5@SEA-D211-03L.birmingham.k12.mi.us>
2016-01-19 15:49:43 -0500

    Edit OI

--- src/org/usfirst/frc/team2960/robot/OI.java
+++ src/org/usfirst/frc/team2960/robot/OI.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team2960.robot;
 
+import org.usfirst.frc.team2960.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 
 /**
@@ -7,9 +10,14 @@ import edu.wpi.first.wpilibj.buttons.Button;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	void driveRobot()
-	{
-		
+	public void driveRobot(Joystick stick, DriveTrain drivetrain) {
+		drivetrain.initDefaultCommand();
+		if(Math.abs(stick.getRawAxis(1)) > 0.1)
+        	drivetrain.move(-stick.getRawAxis(1));
+    	if(stick.getRawAxis(2) > 0.1)
+    		drivetrain.turnRight(stick.getRawAxis(2));
+    	if(stick.getRawAxis(2) < -0.1)
+    		drivetrain.turnLeft(Math.abs(stick.getRawAxis(2)));
 	}
 }
 
