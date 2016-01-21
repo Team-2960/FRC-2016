1337dcf5608d7aa6703bea455560a052fa4cdbcc
andrew <andrew@andrewarpasi.com>
2016-01-21 16:38:30 -0500

    aupdate some uaotoin ode

--- src/org/usfirst/frc/team2960/robot/Autonomous.java
+++ src/org/usfirst/frc/team2960/robot/Autonomous.java
@@ -1,8 +1,5 @@
 package org.usfirst.frc.team2960.robot;
 
-import org.usfirst.frc.team2960.robot.commands.MoveForward;
-
-
 public class Autonomous {
 
 	public static AutonomousCommand getAutonCommandAtIndex(int index)
@@ -11,7 +8,7 @@ public class Autonomous {
 		//switch statement here (or an array of commands)
 	}
 	
-	public static void autonomousUpdate()
+	public static void autonomousUpdate(AutonomousCommand command)
 	{
 		
 	}
