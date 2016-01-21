6b9b177d9dfe926ef68809dbfabcafcff6c7e0f5
Alex <contact@team2960.org>
2016-01-21 15:47:14 -0500

    test for alex

--- src/org/usfirst/frc/team2960/robot/Robot.java
+++ src/org/usfirst/frc/team2960/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-//andrew
+//alex
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
